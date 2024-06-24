cbfb624222eab1ca266aab319478886927448042
Gianluca <dbgian23@gmail.com>
2024-06-25 00:02:55 +0200

    Project started

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+import { BiBed, BiQuestionMark } from "react-icons/bi"
+import { BsQuestionCircle } from "react-icons/bs"
+import { Link } from "react-router-dom"
+import Profile from "./Profile"
+const Navbar = () => {
+    return (
+        <nav className="bg-gray-700 shadow-2xl fixed left-0 right-0 flex md:flex-row md:justify-around justify-around  place-content-center items-center min-h-24">
+            <div className="flex flex-col  ">
+                <Link to="/"> <h1 className="transition-all hover:scale-105 md:text-5xl text-2xl text-green-700 flex justify-center items-center font-bold ">Oasi Verde</h1> </Link>
+                <ul className="flex justify-center space-x-3 md:text-xl  text-white">
+                    <Link to="/rooms" className="transition-all hover:scale-105 flex items-center space-x-1"><BiBed /> <li> Stanze </li ></Link>
+                    <Link to="/about" className="transition-all hover:scale-105 flex items-center space-x-1"><BsQuestionCircle /> <li>About</li></Link>
+                </ul>
+            </div>
+            <Profile />
+        </nav >
+    )
+}
+
+export default Navbar
+
+
---
+++ src/components/Profile.jsx
@@ -0,0 +1,21 @@
+import { FaUser } from "react-icons/fa"
+import { useState } from "react";
+import { Link } from "react-router-dom";
+const Profile = () => {
+    const [dropMenu, setDropMenu] = useState(false);
+    return (
+        <button className="transition-all hover:scale-105 min-h-8 md:text-xl text-white" onMouseOver={() => setDropMenu(true)} onMouseLeave={() => setDropMenu(false)}> <FaUser />
+            {
+                dropMenu &&
+                <ul className="relative">
+                    <div className="absolute rounded-md border border-gray-500 top-1 -end-full bg-gray-600">
+                        <Link to="/login"><li className="hover:bg-green-400 rounded-md">Login</li></Link>
+                        <Link to="/register"> <li className="hover:bg-green-400 rounded-md">Register</li></Link>
+                    </div>
+                </ul>
+            }
+        </button>
+    )
+}
+
+export default Profile
